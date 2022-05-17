9b09ee3276a2f442785e470f18d7fb3fd3a1dc71
dewslyse <dewslyse@gmail.com>
2022-05-17 18:01:56 +0000

    Add action and reducer for total number of cases

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import aggregateReducer from './aggregate/aggregate';
 import countryReducer from './country/country';
 
 const store = configureStore({
   reducer: {
     countries: countryReducer,
+    aggregate: aggregateReducer,
   },
 });
 
