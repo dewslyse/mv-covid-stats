f03c6befd78584c27d81fab209de1d936cc1bde0
dewslyse <dewslyse@gmail.com>
2022-05-17 05:48:01 +0000

    Create action and reducer for country data

---
+++ src/redux/country/country.js
@@ -0,0 +1,39 @@
+import fetchCovidData, { currentDate } from '../../api/api';
+
+const FETCHED_COUNTRIES = 'mv-covid-stats/country/FETCHED_COUNTRIES';
+
+const initialState = [];
+
+export const getCovidData = () => async (dispatch) => {
+  const allData = await fetchCovidData()
+    .then((json) => {
+      const { countries } = json.dates[currentDate];
+
+      return Object.keys(countries).map((key, index) => (
+        {
+          id: index,
+          name: countries[key].name,
+          totalCases: countries[key].today_confirmed,
+          totalDeaths: countries[key].today_deaths,
+        }
+      ));
+    });
+
+  dispatch({
+    type: FETCHED_COUNTRIES,
+    payload: allData,
+  });
+};
+
+const countryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHED_COUNTRIES:
+      console.log(action.payload);
+      return action.payload;
+
+    default:
+      return state;
+  }
+};
+
+export default countryReducer;
