12718c4068cf8230b8d332233da5638199622a57
dewslyse <dewslyse@gmail.com>
2022-05-18 22:33:07 +0000

    Clean up api.js file

--- src/api/api.js
+++ src/api/api.js
@@ -4,15 +4,7 @@ d.setDate(d.getDate() - 1);
 export const currentDate = d.toISOString().slice(0, 10);
 console.log(currentDate);
 
-// const fetchCovidData = () => fetch(covidURL)
-//   .then((response) => response.text())
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log('error', error));
-
-const fetchCovidData = async () => {
-  const response = await fetch(`${covidURL}/${currentDate}`);
-  const data = response.json();
-  return data;
-};
+const fetchCovidData = () => fetch(`${covidURL}/${currentDate}`)
+  .then((response) => response.json());
 
 export default fetchCovidData;
