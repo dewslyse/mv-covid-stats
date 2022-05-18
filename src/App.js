0fe3fe105c570c0824329ac6835130e47b75f733
dewslyse <dewslyse@gmail.com>
2022-05-18 22:43:14 +0000

    Add routes in App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import Countries from './components/Countries';
-import AggregatedData from './components/AggregatedData';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import { getAggregatedData } from './redux/aggregate/aggregate';
+import { getCovidData } from './redux/country/country';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCovidData());
+    dispatch(getAggregatedData());
+  }, []);
+
   return (
-    <div className="App">
-      <AggregatedData />
-      <Countries />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
