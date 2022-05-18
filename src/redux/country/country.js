83a415f4b22f9aaeed210d48b7b4ee17e4e4db30
dewslyse <dewslyse@gmail.com>
2022-05-18 22:41:10 +0000

    Clean up countryReducer file

--- src/redux/country/country.js
+++ src/redux/country/country.js
@@ -4,6 +4,7 @@ const FETCHED_COUNTRIES = 'mv-covid-stats/country/FETCHED_COUNTRIES';
 
 const initialState = [];
 
+// Action Creators
 export const getCovidData = () => async (dispatch) => {
   const allData = await fetchCovidData()
     .then((json) => {
@@ -19,28 +20,16 @@ export const getCovidData = () => async (dispatch) => {
       ));
     });
 
-  // const myNewData = allData.dates[currentDate].countries;
-  // console.log(myNewData);
-
-  // const countries = Object.keys(myNewData).map((key, index) => (
-  //   {
-  //     id: index,
-  //     name: myNewData[key].name,
-  //     totalCases: myNewData[key].today_confirmed,
-  //     totalDeaths: myNewData[key].today_deaths,
-  //   }
-  // ));
-
   dispatch({
     type: FETCHED_COUNTRIES,
     payload: allData,
   });
 };
 
+// Reducer
 const countryReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCHED_COUNTRIES:
-      // console.log(action.payload);
       return action.payload;
 
     default:
