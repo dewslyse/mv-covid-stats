69e152bac92b1b4096c05c38a7e7a61ad13557f8
dewslyse <dewslyse@gmail.com>
2022-05-19 21:45:50 +0000

    Style AggregateData component

--- src/components/AggregatedData.js
+++ src/components/AggregatedData.js
@@ -1,29 +1,36 @@
 import { useSelector } from 'react-redux';
 import { currentDate } from '../api/api';
+import map from '../images/worldmap-mobile.png';
 
 const AggregatedData = () => {
   const total = useSelector((state) => state.aggregate);
 
   return (
-    <>
-      <div>
-        Last Updated:
-        {' '}
-        {currentDate}
+    <section className="hero">
+      <div className="hero-content">
+        <div className="map">
+          <img src={map} alt="" />
+        </div>
+        <div className="aggregates">
+          <h2>World Covid-19 stats</h2>
+          <p>
+            Last Updated:
+            {' '}
+            {currentDate}
+          </p>
+          <h3>
+            Total cases:
+            {' '}
+            <span>{total.today_confirmed}</span>
+          </h3>
+          <h3>
+            Total deaths:
+            {' '}
+            <span>{total.today_deaths}</span>
+          </h3>
+        </div>
       </div>
-      <div>
-        <h3>
-          <span>Total cases:</span>
-          {' '}
-          {total.today_confirmed}
-        </h3>
-        <h3>
-          <span>Total deaths:</span>
-          {' '}
-          {total.today_deaths}
-        </h3>
-      </div>
-    </>
+    </section>
   );
 };
 
