0d63b1890eb6f05469bdb775e0bac10907308258
dewslyse <dewslyse@gmail.com>
2022-06-05 20:08:54 +0000

    FIX: Adjust current date for api call

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 const covidURL = 'https://api.covid19tracking.narrativa.com/api';
 const d = new Date();
-d.setDate(d.getDate() - 1);
+d.setDate(d.getDate() - 3);
 export const currentDate = d.toISOString().slice(0, 10);
 
 const fetchCovidData = () => fetch(`${covidURL}/${currentDate}`)
